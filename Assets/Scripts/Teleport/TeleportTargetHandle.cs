a4dd65538fedae2c16a828f5d0b9907720203bcd
Ruszard <nekorushi@gmail.com>
2018-11-10 02:25:11 +0100

    Teleport player on collision with portal

--- Assets/Scripts/Teleport/TeleportTargetHandle.cs
+++ Assets/Scripts/Teleport/TeleportTargetHandle.cs
@@ -7,18 +7,22 @@ public class TeleportTargetHandle : Editor {
     protected virtual void OnSceneGUI () {
         Teleport teleport = (Teleport)target;
 
-        float size = HandleUtility.GetHandleSize(teleport.destination) * 0.3f;
-
         EditorGUI.BeginChangeCheck();
         Handles.color = Color.magenta;
-        Vector3 teleportDestination = Handles.FreeMoveHandle(teleport.destination, Quaternion.identity, size, Vector3.one, Handles.SphereHandleCap);
+        Vector3 teleportDestination = Handles.FreeMoveHandle(
+            teleport.Destination, 
+            Quaternion.identity, 
+            0.3f, 
+            Vector3.one, 
+            Handles.SphereHandleCap
+        );
 
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(teleport, "Change teleport destination.");
-            teleport.destination = new Vector3(
+            teleport.Destination = new Vector3(
                     Mathf.Round(teleportDestination.x),
-                    Mathf.Round(teleport.destination.y),
+                    Mathf.Round(teleport.Destination.y),
                     Mathf.Round(teleportDestination.z)
                 );
         }
