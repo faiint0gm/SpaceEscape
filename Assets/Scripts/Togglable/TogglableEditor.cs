5ca24e0de0e9bee48b8f9dcb0bffc821347ce20e
Ruszard <nekorushi@gmail.com>
2018-11-10 17:59:40 +0100

    Add switch console

---
+++ Assets/Scripts/Togglable/TogglableEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Togglable))]
+public class TogglableEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ITogglable togglable = (ITogglable)target;
+        if (GUILayout.Button("Toggle"))
+        {
+            togglable.Toggle();
+        }
+    }
+}
