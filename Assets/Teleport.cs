a2cab933694fb21c2bad2268a149c34d1f5def96
Ruszard <nekorushi@gmail.com>
2018-11-10 01:52:12 +0100

    Add basic teleport

---
+++ Assets/Teleport.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+    public Vector3 destination { get { return m_destination; } set { m_destination = value; } }
+    [SerializeField]
+    private Vector3 m_destination = new Vector3(1f, 0f, 0f);
+}
