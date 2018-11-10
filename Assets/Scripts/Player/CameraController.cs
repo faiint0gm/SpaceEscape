422e67a8ec85af06653247a0bbcbcc48903241d6
Piotr Rudnicki <p.rudnicki@developerodzera.pl>
2018-11-10 01:51:44 +0100

    Add PlayerController and CameraController

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 targetPositionOffset = new Vector3(21.3f,30,0);
+    public float smooth = 0.05f;
+
+    Vector3 targetPos = Vector3.zero;
+    Vector3 destination = Vector3.zero;
+
+    private void Start()
+    {
+        SetCameraTarget(target);
+
+        MoveToTarget();
+    }
+
+    private void FixedUpdate()
+    {
+        //moving
+        MoveToTarget();
+        LookAtTarget();
+    }
+
+    void SetCameraTarget(Transform t)
+    {
+        target = t;
+
+        if (target == null || !target.GetComponent<PlayerController>())
+        {
+            Debug.LogError("The target needs a PlayerController script!");
+        }
+    }
+
+    void MoveToTarget()
+    {
+        targetPos = target.position + targetPositionOffset;
+        transform.position = targetPos;
+    }
+
+    void LookAtTarget()
+    {
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [Range(100,300)]
+    public float rotationSpeed;
+
+    [Range(1,10)]
+    public float movementSpeed;
+
+    private void Update()
+    {
+        CheckMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            Interact();
+    }
+
+    void CheckMovement()
+    {
+        float translation = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        float rotation = rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Translate(0, 0, translation);
+        }
+        else
+            transform.Translate(0, 0, 0);
+    }
+
+    void Interact()
+    {
+        Debug.Log("Interaction");
+    }
+}
