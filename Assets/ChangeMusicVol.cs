0a0485bc5ac6c2cb78a325dd0b54de3ea89b273b
Paolo93 <ratalewski@wp.pl>
2018-11-10 05:09:21 +0100

    mixer and menu

---
+++ Assets/ChangeMusicVol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class ChangeMusicVol : MonoBehaviour {
+
+    public AudioMixer mainMixer;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    public void SetMusicLvl (float musicLvl)
+    {
+        mainMixer.SetFloat("musicVol", musicLvl);
+    }
+
+    public void ClearVolume()
+    {
+        mainMixer.ClearFloat("musicVol");
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+  
+}
---
+++ Assets/SelectableMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class SelectableMenu : MonoBehaviour
+{
+
+    GameObject lastselect;
+    //public Button startButton;
+    //int minValue = 0, maxValue = 100, variationAmount = 2;
+    //public Slider sliderRef;
+
+
+    void Start()
+    {
+        lastselect = new GameObject();
+        EventSystem.current.SetSelectedGameObject(EventSystem.current.firstSelectedGameObject);
+
+        /*sliderRef = this.gameObject.GetComponent<Slider>();
+        sliderRef.minValue = minValue;
+        sliderRef.maxValue = maxValue;*/
+
+    }
+
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastselect);
+        }
+        else
+        {
+            lastselect = EventSystem.current.currentSelectedGameObject;
+        }
+
+       /* if (Input.GetKeyDown("a"))
+        {
+
+            sliderRef.value -= variationAmount;
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            sliderRef.value += variationAmount;
+        }*/
+
+
+    }
+}
