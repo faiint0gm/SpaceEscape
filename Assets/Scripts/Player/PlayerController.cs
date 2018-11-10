a9e938b70bf1c89b7b704c7effba2e7e63920f26
Piotr Rudnicki <p.rudnicki@developerodzera.pl>
2018-11-10 03:51:31 +0100

    Update PlayerController script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,33 +10,111 @@ public class PlayerController : MonoBehaviour {
     [Range(1,10)]
     public float movementSpeed;
 
+    float direction;
+    float time;
+    private void Start()
+    {
+        direction = 0;
+        time = 0;
+    }
+
     private void Update()
     {
-        CheckMovement();
+        //CheckMovement();
     }
 
     private void FixedUpdate()
     {
+        CheckMovement();
         if (Input.GetKeyDown(KeyCode.E))
             Interact();
     }
 
     void CheckMovement()
     {
-        float translation = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        float rotation = rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
 
-        if (Input.GetAxis("Vertical") != 0)
-        {
-            transform.Translate(0, 0, translation);
-        }
-        else
+        if (Input.GetAxisRaw("Vertical") == 0 && Input.GetAxisRaw("Horizontal") == 0)
             transform.Translate(0, 0, 0);
+        else
+        {
+            Move(Pos(Input.GetAxisRaw ("Vertical"), Input.GetAxisRaw("Horizontal")));
+        }
     }
 
     void Interact()
     {
         Debug.Log("Interaction");
+    }
+
+    int Pos(float ver, float hor)
+    {
+        int pos = -1;
+        if (ver > 0 && hor == 0)
+            pos = 0;
+        else if (ver < 0 && hor == 0)
+            pos = 1;
+        else if (ver == 0 && hor > 0)
+            pos = 2;
+        else if (ver == 0 && hor < 0)
+            pos = 3;
+        else if (ver > 0 && hor > 0)
+            pos = 4;
+        else if (ver < 0 && hor > 0)
+            pos = 5;
+        else if (ver < 0 && hor < 0)
+            pos = 6;
+        else if (ver > 0 && hor < 0)
+            pos = 7;
+        return pos;
+    }
+
+    void Move(int position)
+    {
+        float translationVer = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+        float translationHor = Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;
+
+        switch (position)
+        {
+            case 0:
+                direction = 0;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+                transform.Translate(0, 0, translationVer);
+                break;
+            case 1:
+                direction = 180;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+               transform.Translate(0, 0, -translationVer);
+                break;
+            case 2:
+                direction = 90;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+                transform.Translate(0, 0, translationHor);
+                break;
+            case 3:
+                direction = 270;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+                transform.Translate(0, 0, -translationHor);
+                break;
+            case 4:
+                direction = 45;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+                transform.Translate(0, 0, translationVer);
+                break;
+            case 5:
+                direction = 135;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+                transform.Translate(0, 0, -translationVer);
+                break;
+            case 6:
+                direction = 225;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+                transform.Translate(0, 0, -translationVer);
+                break;
+            case 7:
+                direction = 315;
+                transform.rotation = Quaternion.Euler(0, direction, 0);
+                transform.Translate(0, 0, translationVer);
+                break;
+        }
     }
 }
