f6d01df99298acd7eca8ea6bd7f30a9f80ee6d48
Piotr Rudnicki <p.rudnicki@developerodzera.pl>
2018-11-10 16:30:04 +0100

    Add NavMeshComponents. Set opponent patrol mode

---
+++ Assets/Scripts/Opponents/PathPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathPoint : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.GetComponent<Patrol>().enabled)
+            {
+                other.gameObject.GetComponent<OpponentController>().actualAngle = transform.rotation.y;
+                StartCoroutine(other.gameObject.GetComponent<OpponentController>().Rotate());
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Opponents/Patrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    public OpponentController opponentController;
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    public object Go { get; private set; }
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GetComponent<OpponentController>().actualAngle = transform.rotation.y;
+        StartCoroutine(opponentController.Rotate());
+    }
+
+
+    public void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+         
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+}
