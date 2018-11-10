669ff5a2987a0a23217c29a8457f1532b6e9237e
Ruszard <nekorushi@gmail.com>
2018-11-10 10:55:57 +0100

    Add basic portal structure

---
+++ Assets/Scripts/Portal/PortalTargetHandle.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Portal))]
+public class PortalTargetHandle : Editor
+{
+
+    protected virtual void OnSceneGUI ()
+    {
+        Portal portal = (Portal)target;
+        Transform entry = portal.entry;
+        Transform exit = portal.exit;
+
+        EditorGUI.BeginChangeCheck();
+
+        Handles.color = Color.cyan;
+        Vector3 entryDestination = Handles.PositionHandle(
+            entry.position + new Vector3(0, 2f, 0),
+            Quaternion.identity
+        );
+
+        Vector3 exitDestination = Handles.PositionHandle(
+            exit.position + new Vector3(0, 2f, 0),
+            Quaternion.identity
+        );
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(portal, "Change portal destination.");
+            entry.position = new Vector3(
+                    Mathf.Round(entryDestination.x),
+                    0,
+                    Mathf.Round(entryDestination.z)
+                );
+            exit.position = new Vector3(
+                    Mathf.Round(exitDestination.x),
+                    0,
+                    Mathf.Round(exitDestination.z)
+                );
+        }
+    }
+}
